02da32d5790c36bb7eb46b1f38436a5a2f203727
akuyper <a-kuyper@northwestern.edu>
2024-09-05 00:42:36 -0500

    State analysis script complete

--- 1_state_analysis.R
+++ 1_state_analysis.R
@@ -6,6 +6,7 @@ library(tigris)
 library(tidycensus)
 library(sf)
 library(statebins)
+library(patchwork)
 
 # load data ----
 
@@ -13,11 +14,27 @@ library(statebins)
 state_cwift <- read_tsv("data/EDGE_ACS_CWIFT2021_State.txt") |> 
   janitor::clean_names()
 
-# county map data
+# top 5 states
+state_cwift |> 
+  slice_max(st_cwiftest, n = 5)
+
+# bottom 5 states
+state_cwift |> 
+  slice_min(st_cwiftest, n = 5)
+
+# state map data
 state_map_data <- tigris::states(year = 2021, cb = TRUE) |> 
   shift_geometry() |> 
   janitor::clean_names() |> 
   filter(statefp %in% unique(state_cwift$st_fips))
+
+# join state CWIFT data with mapping/geographic data
+state_cwift_map_data <- state_map_data |> 
+  left_join(
+    state_cwift |> 
+      select(st_fips, st_cwiftest, st_cwiftse),
+    by = c("geoid" = "st_fips")
+  ) 
 
 # state household income data from ACS
 income_data <- get_acs(
@@ -35,16 +52,22 @@ state_cwift |>
   skimr::skim_without_charts()
 
 # inspect county CWIFT estimates
-(cwfti_estimates <- state_cwift |> 
-  mutate(st_name = fct_reorder(factor(st_name), st_cwiftest, .fun = median, .na_rm = TRUE)) |> 
+(cwift_estimates <- state_cwift |> 
+  mutate(
+    st_name = fct_reorder(
+      factor(st_name), 
+      st_cwiftest, 
+      .fun = median, 
+      .na_rm = TRUE)
+    ) |> 
   ggplot(aes(st_cwiftest, st_name)) +
   geom_errorbar(
-    aes(
+    mapping = aes(
       xmin = st_cwiftest - 2*st_cwiftse,
       xmax = st_cwiftest + 2*st_cwiftse
     ),
     width = 0.3
-  ) + 
+    ) + 
   geom_vline(xintercept = 1, linetype = "dashed") +
   theme_classic() +
   labs(
@@ -52,8 +75,10 @@ state_cwift |>
     y = NULL
   ))
 
+# univariate graphs fo state CWIFT
 
-state_cwift |> 
+# dot density plot
+(dot_density_cwift <- state_cwift |> 
   ggplot(aes(st_cwiftest)) +
   ggdist::geom_dots() +
   theme_classic(base_size = 14) +
@@ -61,52 +86,65 @@ state_cwift |>
     axis.text.y = element_blank(),
     axis.ticks.y = element_blank()
   ) +
-  xlab("State CWIFT") 
+  xlab("State CWIFT"))
 
-state_cwift |> 
+# box plot
+(box_plot_cwift <- state_cwift |> 
   ggplot(aes(st_cwiftest)) +
-  geom_boxplot() 
+  geom_boxplot())
 
-state_cwift |> 
+# density plot
+(density_plot_cwift <-state_cwift |> 
   ggplot(aes(st_cwiftest)) +
-  geom_density() 
+  geom_density())
 
-state_cwift_map_data <- state_map_data |> 
-  left_join(
-    state_cwift |> 
-      select(st_fips, st_cwiftest, st_cwiftse),
-    by = c("geoid" = "st_fips")
-  ) 
-
-haha <- state_cwift_map_data |> 
+# CWIFT state heat map
+(state_cwift_heat_map <- state_cwift_map_data |> 
   ggplot(aes(fill = st_cwiftest), color = NA) +
   geom_sf() +
   scale_fill_distiller(
+    name = "State\nCWIFT",
     type = "seq",
     direction = 1,
     palette = "Greys", limits = c(0.75, 1.5)
   ) +
-  theme_void()
+  theme_void())
 
-state_cwift_map_data |> 
+# Heat map with states in Census defined West region
+# list of states in Census defined west region
+census_west_region_states <- tibble(state_abb = state.abb, region = as.character(state.region)) |> 
+  filter(region == "West") |> 
+  pull(state_abb)
+
+# heat map
+(west_state_cwift_heat_map <- state_cwift_map_data |> 
   filter(
-    stusps %in% c("NJ", "NY", "PA")
+    stusps %in% c(census_west_region_states)
   ) |> 
   ggplot(aes(fill = st_cwiftest), color = NA) +
   geom_sf() +
+  geom_sf_label(aes(label = stusps)) +
   scale_fill_distiller(
+    name = "State\nCWIFT",
     type = "seq",
     direction = 1,
     palette = "Greys", limits = c(0.75, 1.5)
   ) +
-  theme_void()
+  theme_void())
 
+# statebins CWIFT heat map
+(st_cwift_bins_plot <- state_cwift |> 
+    ggplot(aes(state = st_name, fill = st_cwiftest)) +
+    geom_statebins() +
+    scale_fill_distiller(
+      name = "State\nCWIFT",
+      type = "seq",
+      direction = 1,
+      palette = "Greys", limits = c(0.75, 1.5)
+    ) +
+    theme_void())
 
-ggsave(filename = "test.pdf", plot = haha, width = 11.5, height = 8)
-
-
-
-# state cwfti by houeshold income
+# state cwift by houeshold income
 (cwift_by_income <- income_data |> 
   inner_join(
     state_cwift |> 
@@ -123,15 +161,32 @@ ggsave(filename = "test.pdf", plot = haha, width = 11.5, height = 8)
   ylab("State CWIFT") +
   theme_minimal())
 
-# statebins CWIFT map
-(st_wwift_bins_plot <- state_cwift |> 
-  ggplot(aes(state = st_name, fill = st_cwiftest)) +
-  geom_statebins() +
-  scale_fill_distiller(
-    name = "State\nCWIFT",
-    type = "seq",
-    direction = 1,
-    palette = "Greys", limits = c(0.75, 1.5)
-  ) +
-  theme_statebins() )
-  
+# arranging plots & writing/saving plots out for book chapter
+
+# Example CWIFT plots
+(state_plot_collection <- cwift_estimates + 
+  (density_plot_cwift / box_plot_cwift  / dot_density_cwift) +
+  plot_annotation(tag_levels = "A"))
+
+# CWIFT heat maps
+(state_heat_maps <- (state_cwift_heat_map + st_cwift_bins_plot) +
+  plot_annotation(tag_levels = "A") +
+  plot_layout(guides = "collect") &
+  theme(legend.position = "bottom"))
+
+# write out/save images
+ggsave(
+  filename = "plots/state_plot_collection.png",
+  plot = state_plot_collection,
+  height= 7,
+  width = 6,
+  units = "in"
+  )
+
+ggsave(
+  filename = "plots/state_heatmaps.png",
+  plot = state_heat_maps,
+  height= 3,
+  width = 8,
+  units = "in"
+)
